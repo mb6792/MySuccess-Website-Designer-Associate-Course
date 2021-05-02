f975184c0064bf8a53a3aba4f70992e9b24b60eb
Mark Bonnici <mb6792@gmail.com>
2021-05-02 16:18:03 +0200

    Module 4 Topic 2

---
+++ lesson2/lesson2.js
@@ -0,0 +1,78 @@
+document.getElementById("mainDiv").innerHTML = "Hello, World!";
+document.write("This is a test");
+//alert('Hello there!');
+document.write("<br>Another test");
+console.log('Hello from the console!')
+
+
+var num1, num2, sum;
+num1 = 10;
+num2 = 5;
+sum = num1 + num2;
+console.log(sum);
+
+var text1 = "This one doesn't have spaces";
+var text2 = "This one has spaces";
+var text3 = "This one has spaces             inside a string literal";
+console.log(text1);
+console.log(text2);
+console.log(text3);
+
+document.getElementById("mainDiv").innerHTML = "Text Changed!";
+
+function someFunction() {
+    console.log("This is a statement.");
+    console.log("So is this one.")
+}
+
+someFunction();
+
+for (let i = 0; i < 3; i++) {
+    console.log("Inside Loop:" + i);
+}
+// console.log("Outside Loop: " + i);
+
+const PI = 3.14159;
+console.log(PI);
+// PI = 22 / 7;
+
+
+let res = 8 * 90;
+console.log(res);
+
+let user = "Joe";
+let greeting = "Good Afternoon ";
+let phrase = greeting + user;
+console.log(phrase);
+
+// let name = "Greg";
+// console.log(Name);
+
+/*
+    This is a multi-line comment.
+    We can use this when we have more to say, or to better explain
+    our code.
+*/
+let badvariablename = 1; // Bad variable name
+let goodVariableName = 2; // Good variable name
+
+console.log('run me!');
+// console.log('do not run me!');
+console.log('run me!');
+
+let type1 = "Hello!"; // string
+let type2 = 'Hello!'; // string
+let type3 = "47"; // string
+let type4 = 47; // number
+
+var person = "Kate Wilkins", carName = "Audi", currTemp = 23.1;
+
+var pName = "John";
+let pName2 = "Bob";
+pName = "Sue";
+pName2 = "Kate";
+console.log(pName);
+console.log(pName2);
+
+var pName = "Frank";
+// let pName2 = "Dennis";

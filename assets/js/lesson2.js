ac200a999f9a44122aafa6db65a6da56d2a6123e
Mark Bonnici <mb6792@gmail.com>
2021-06-10 13:31:07 +0200

    6th Lesson

---
+++ assets/js/lesson2.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+document.getElementById('mainDiv').innerHTML = "Hello, World!";
+document.write('This is a test');
+
+//alert('Hello there!');
+document.write('<br> Another test');
+
+console.log('Hello from the console!');
+
+
+
+var num1, num2, sum;
+num1 = 10;
+num2 = 5;
+sum = num1 + num2;
+console.log(sum);
+
+
+
+var text1="This one doesn't have spaces";
+var text2 = "This one has spaces";
+var text3 = "This one has spaces          inside string literal."
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
+
+
+document.getElementById("mainDiv")
+  .innerHTML = "Text Changed!";
+
+
+function someFunction(){
+    console.log("This is a statement.");
+    console.log('So is this one.');
+}
+someFunction();
+
+
+
